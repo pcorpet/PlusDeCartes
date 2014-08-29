e3b57aeb5958d147003f9c915b3a6564be906831
zozoens31 <cyrille@corpet.net>
2014-08-29 11:45:54 +0200

    CarteServlet récupère une carte au hasard (buggé)

--- src/net/corpet/plusdecartes/serveur/CarteServlet.java
+++ src/net/corpet/plusdecartes/serveur/CarteServlet.java
@@ -1,10 +1,21 @@
 package net.corpet.plusdecartes.serveur;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.Random;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 
 /**
@@ -16,11 +27,31 @@ public class CarteServlet extends HttpServlet {
 	@Override
     public void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws IOException {
-		String nom = req.getQueryString();
-		if (nom == null) {
-			nom = "Monde";
-		}
-        resp.setContentType("text/plain");
-        resp.getWriter().println("{ \"nom\": \"" + nom  + "\" }");
+		
+		resp.setContentType("text/plain");
+        DatastoreService base = DatastoreServiceFactory.getDatastoreService();
+        Random de = new Random();
+        
+        double seuil = de.nextDouble();
+        Filter filtreParDefaut = 
+        		new FilterPredicate("ordreHasard",Query.FilterOperator.GREATER_THAN, seuil);
+        
+        Query q = new Query("concept").setFilter(filtreParDefaut).addSort("ordreHasard");
+        PreparedQuery pq = base.prepare(q);
+        
+        /*if (pq.countEntities(FetchOptions.Builder.withLimit(1))==0){
+        	q = new Query("concept").addSort("ordreHasard");
+        	pq = base.prepare(q);
+        }*/
+        
+        resp.getWriter().println(seuil);        
+        List<Entity> concepts = pq.asList(FetchOptions.Builder.withLimit(1));
+        if (concepts.isEmpty()){
+        	resp.getWriter().println("Il n'y a pas d'entité correspondant à votre requête.");
+        } else {
+        	String nom = (String) concepts.get(0).getProperty("concept");
+            resp.getWriter().println("Voici un concept tiré au hasard dans la base : " + nom);
+        }
+
     }
 }
