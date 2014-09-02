1a8047aa82910b876fa5a2f0e721bbcd42749828
zozoens31 <cyrille@corpet.net>
2014-09-02 14:35:53 +0200

    objectification du code

--- src/net/corpet/plusdecartes/serveur/CarteServlet.java
+++ src/net/corpet/plusdecartes/serveur/CarteServlet.java
@@ -3,7 +3,6 @@ package net.corpet.plusdecartes.serveur;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Random;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,30 +10,11 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.PreparedQuery;
-import com.google.appengine.api.datastore.Query;
-import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
-import com.google.appengine.api.datastore.Query.Filter;
-import com.google.appengine.api.datastore.Query.FilterPredicate;
-import com.google.appengine.api.datastore.QueryResultList;
-
 
 /**
  * L'implémentation côté serveur du service
  */
-@SuppressWarnings("serial")
-class ExceptionPasAssezDEntites extends Throwable{
-	Filter filtre;
-	List<Key> liste;
-	
-	public ExceptionPasAssezDEntites(Filter f, List<Key> l){
-		filtre = f;
-		liste = l;
-	}
-}
 
 @SuppressWarnings("serial")
 public class CarteServlet extends HttpServlet {
@@ -51,59 +31,18 @@ public class CarteServlet extends HttpServlet {
 			nombre = Integer.parseInt(req.getParameter("nombre"));
 		} catch (NumberFormatException e) {}
 		
-		for (int i=0;i<nombre;i++){
-			try {
-				cles.add(cleConceptAuHasard(base, cles));
-			} catch (ExceptionPasAssezDEntites e) {
-				resp.getWriter().println("Il n'y a que " + e.liste.size() + " correspondant à la requête.");
-			}
+		Requete requete = new Requete();
+		
+		try {
+			for (int i=0;i<nombre;i++){
+				cles.add(requete.conceptSuivant(base).getCle());
+				}
+		} catch (ExceptionPasAssezDEntites e) {
+			resp.getWriter().println("Il n'y a que " + e.clesDisponibles.size() + " résultats correspondant à la requête.");
 		}
 		resp.getWriter().println("Voici "+ cles.size() + " concept(s) tiré(s) au hasard dans la base : ");
 		for (Key cle : cles){
 			resp.getWriter().println(cle.getName());
 		}		
-	}
-	
-	private Key cleConceptAuHasard(DatastoreService base, List<Key> interdites) throws ExceptionPasAssezDEntites {
-		return cleConceptAuHasard(base, interdites, null);
-	}
-	private Key cleConceptAuHasard(DatastoreService base, List<Key> interdites, Filter filtre) 
-			throws ExceptionPasAssezDEntites {
-		Random de = new Random();
-        double seuil = de.nextDouble();
-        
-        FilterPredicate filtreParDefaut = 
-        		new FilterPredicate("ordreHasard",Query.FilterOperator.GREATER_THAN, seuil);
-        Filter filtreFinal;
-        if (filtre==null){
-        	filtreFinal = filtreParDefaut;
-        } else {
-        	filtreFinal = CompositeFilterOperator.and(filtre, filtreParDefaut);
-        }
-        Query q = new Query("Concept").setKeysOnly().setFilter(filtreFinal).addSort("ordreHasard");
-        PreparedQuery pq = base.prepare(q);
-        
-        FetchOptions fetchOptions = FetchOptions.Builder.withLimit(1);
-        QueryResultList<Entity> concepts = pq.asQueryResultList(fetchOptions);
-        Key res = null;
-        boolean boucle = false;
-        do {
-        	try {
-        		res = concepts.get(0).getKey();
-        	}
-        	catch (IndexOutOfBoundsException e) {
-        		if (boucle) {
-        			throw new ExceptionPasAssezDEntites(filtre, interdites);
-        		}
-        		q.setFilter(filtre);
-        		pq = base.prepare(q);
-        		fetchOptions = FetchOptions.Builder.withLimit(1);
-        		concepts = pq.asQueryResultList(fetchOptions);
-        		boucle = true;
-        	}
-        	fetchOptions.startCursor(concepts.getCursor());
-        	concepts = pq.asQueryResultList(fetchOptions);
-        } while (res==null || interdites.contains(res));
-        return res;
-	}
+	}	
 }
---
+++ src/net/corpet/plusdecartes/serveur/Concept.java
@@ -0,0 +1,104 @@
+package net.corpet.plusdecartes.serveur;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.FetchOptions;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.QueryResultList;
+import com.google.appengine.api.datastore.Query.CompositeFilterOperator;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+
+@SuppressWarnings("serial")
+class ExceptionPasAssezDEntites extends Throwable{
+	Filter filtre;
+	Set<Key> clesDisponibles;
+	
+	public ExceptionPasAssezDEntites(Filter f, Set<Key> l){
+		filtre = f;
+		clesDisponibles = l;
+	}
+}
+
+class Concept {
+	private Key cle;
+	private Entity entite = null;
+
+	// Constructeur à partir d'une clé DataStore
+	public Concept(Key c){
+		cle = c;
+	}
+	
+	public Key getCle(){
+		return cle;
+	}
+	public Entity getEntite(){
+		return entite;
+	}
+	
+	public void recupEntite(DatastoreService base) throws EntityNotFoundException{
+		if (entite==null){
+			entite = base.get(cle);
+		}
+	}
+}
+
+class Requete {
+	private Filter filtre;
+	private Set<Key> clesDejaTirees = new HashSet<Key>();
+	
+	public Requete(){
+		filtre = null;
+	}
+	public Requete(Filter f){
+		filtre = f;
+	}
+	
+	public void reinitClesTirees(){
+		clesDejaTirees = new HashSet<Key>();
+	}
+	
+	public Concept conceptSuivant(DatastoreService base) throws ExceptionPasAssezDEntites {
+		Random de = new Random();
+        double seuil = de.nextDouble();
+        
+        FilterPredicate filtreParDefaut = 
+        		new FilterPredicate("ordreHasard",Query.FilterOperator.GREATER_THAN, seuil);
+        Filter filtreFinal = filtre==null?filtreParDefaut:CompositeFilterOperator.and(filtre, filtreParDefaut);
+        
+       	Query q = new Query("Concept").setKeysOnly().setFilter(filtreFinal).addSort("ordreHasard");
+        PreparedQuery pq = base.prepare(q);
+        FetchOptions fetchOptions = FetchOptions.Builder.withLimit(1);
+        QueryResultList<Entity> concepts = pq.asQueryResultList(fetchOptions);
+
+        Key res = null;
+        boolean boucle = false;
+        do {
+        	try {
+        		res = concepts.get(0).getKey();
+        	}
+        	catch (IndexOutOfBoundsException e) {
+        		if (boucle) {
+        			throw new ExceptionPasAssezDEntites(filtre, clesDejaTirees);
+        		}
+        		q.setFilter(filtre);
+        		pq = base.prepare(q);
+        		fetchOptions = FetchOptions.Builder.withLimit(1);
+        		concepts = pq.asQueryResultList(fetchOptions);
+        		boucle = true;
+        	}
+        	
+        	fetchOptions.startCursor(concepts.getCursor());
+        	concepts = pq.asQueryResultList(fetchOptions);
+        } while (res==null || clesDejaTirees.contains(res));
+        clesDejaTirees.add(res);
+        return new Concept(res);
+	}
+}
