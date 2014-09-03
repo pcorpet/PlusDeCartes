dc0d6044d234eedf2d650dea1bc487e8df759ac5
pcorpet <pascal+github@corpet.net>
2014-09-03 21:43:55 +0200

    Nettoie CarteServlet.java
    Remplace les Tab par des indentations d'espaces (2 espaces). Corrige toute l'indentation

--- src/net/corpet/plusdecartes/serveur/CarteServlet.java
+++ src/net/corpet/plusdecartes/serveur/CarteServlet.java
@@ -22,65 +22,62 @@ import com.google.appengine.api.datastore.Query.FilterPredicate;
 
 
 /**
- * L'implémentation côté serveur du service
+ * L'implémentation côté serveur du service.
  */
 @SuppressWarnings("serial")
-class ExceptionPasDEntite extends Throwable{
-	Filter filtre;
-	
-	public ExceptionPasDEntite(Filter f){
-		filtre = f;
-	}
+class ExceptionPasDEntite extends Throwable {
+  Filter filtre;
+  
+  public ExceptionPasDEntite(Filter f) {
+    filtre = f;
+  }
 }
 
 @SuppressWarnings("serial")
 public class CarteServlet extends HttpServlet {
 
-	@Override
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-		
-		resp.setContentType("text/plain");
-		DatastoreService base = DatastoreServiceFactory.getDatastoreService();
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+    resp.setContentType("text/plain");
+    DatastoreService base = DatastoreServiceFactory.getDatastoreService();
         
-		try {
-			String nom = (String) base.get(cleConceptAuHasard(base)).getProperty("concept");
-			resp.getWriter().println("Voici un concept tiré au hasard dans la base : " + nom);
-		} catch (ExceptionPasDEntite|EntityNotFoundException e) {
-			resp.getWriter().println("Il n'y a pas de concept correspondant à la requête");
-	        
-		}
-		
-	}
-	
-	private Key cleConceptAuHasard(DatastoreService base) throws ExceptionPasDEntite {
-		return cleConceptAuHasard(base, null);
-	}
-	private Key cleConceptAuHasard(DatastoreService base, Filter filtre) throws ExceptionPasDEntite {
-		Random de = new Random();
-        double seuil = de.nextDouble();
+    try {
+      String nom = (String) base.get(cleConceptAuHasard(base)).getProperty("concept");
+      resp.getWriter().println("Voici un concept tiré au hasard dans la base : " + nom);
+    } catch (ExceptionPasDEntite|EntityNotFoundException e) {
+      resp.getWriter().println("Il n'y a pas de concept correspondant à la requête");
+    }
+  }
+  
+  private Key cleConceptAuHasard(DatastoreService base) throws ExceptionPasDEntite {
+    return cleConceptAuHasard(base, null);
+  }
+
+  private Key cleConceptAuHasard(DatastoreService base, Filter filtre) throws ExceptionPasDEntite {
+    Random de = new Random();
+    double seuil = de.nextDouble();
         
-        FilterPredicate filtreParDefaut = 
-        		new FilterPredicate("ordreHasard",Query.FilterOperator.GREATER_THAN, seuil);
-        Filter filtreFinal;
-        if (filtre==null){
-        	filtreFinal = filtreParDefaut;
-        } else {
-        	filtreFinal = CompositeFilterOperator.and(filtre, filtreParDefaut);
-        }
-        Query q = new Query("Concept").setKeysOnly().setFilter(filtreFinal).addSort("ordreHasard");
-        PreparedQuery pq = base.prepare(q);
+    FilterPredicate filtreParDefaut = 
+        new FilterPredicate("ordreHasard",Query.FilterOperator.GREATER_THAN, seuil);
+    Filter filtreFinal;
+    if (filtre==null){
+      filtreFinal = filtreParDefaut;
+    } else {
+      filtreFinal = CompositeFilterOperator.and(filtre, filtreParDefaut);
+    }
+    Query q = new Query("Concept").setKeysOnly().setFilter(filtreFinal).addSort("ordreHasard");
+    PreparedQuery pq = base.prepare(q);
         
-        if (pq.countEntities(FetchOptions.Builder.withLimit(1))==0){
-        	q.setFilter(filtre);
-        	pq = base.prepare(q);
-        }
+    if (pq.countEntities(FetchOptions.Builder.withLimit(1))==0){
+      q.setFilter(filtre);
+      pq = base.prepare(q);
+    }
         
-        List<Entity> concepts = pq.asList(FetchOptions.Builder.withLimit(1));
-        if (concepts.isEmpty()){
-        	throw new ExceptionPasDEntite(filtre);
-        } else {
-        	return concepts.get(0).getKey();
-        }
-	}
+    List<Entity> concepts = pq.asList(FetchOptions.Builder.withLimit(1));
+    if (concepts.isEmpty()){
+      throw new ExceptionPasDEntite(filtre);
+    } else {
+      return concepts.get(0).getKey();
+    }
+  }
 }
